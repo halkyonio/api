e90a6bb6d822720e44b6e220f5d73dfdfc119829
Chris Laprun <metacosm@gmail.com>
2019-03-01 11:09:31 +0100

    Add k8s common label constants.

--- pkg/apis/component/v1alpha1/component_types.go
+++ pkg/apis/component/v1alpha1/component_types.go
@@ -94,6 +94,16 @@ var phases = []string{
 }
 
 const (
+	// See https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/
+	NameLabelKey           = "app.kubernetes.io/name"
+	VersionLabelKey        = "app.kubernetes.io/version"
+	InstanceLabelKey       = "app.kubernetes.io/instance"
+	PartOfLabelKey         = "app.kubernetes.io/part-of"
+	ComponentLabelKey      = "app.kubernetes.io/component"
+	ManagedByLabelKey      = "app.kubernetes.io/managed-by"
+	RuntimeLabelKey        = "app.openshift.io/runtime"
+	RuntimeVersionLabelKey = "app.openshift.io/version"
+
 	Unknown Phase = iota
 	ServiceCreation
 	Building
