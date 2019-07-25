ab22393e9f04817a2d73dd5416824d60cca4a30f
Chris Laprun <metacosm@gmail.com>
2019-07-25 11:49:19 +0200

    feat: add v1alpha2 version

---
+++ pkg/apis/component/v1alpha2/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha2 contains API Schema definitions for the Component v1alpha2 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=devexp.runtime.redhat.com
+package v1alpha2
